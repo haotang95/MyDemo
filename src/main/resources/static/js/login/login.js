060a07d14149d6b4fdd03c47e4d7be4e141fefef
tangh <suzhoutanghao@hotmail.com>
2019-08-08 17:12:33 +0800

    modify

--- src/main/resources/static/js/login/login.js
+++ src/main/resources/static/js/login/login.js
@@ -1,17 +1,19 @@
-$("#loginBtn").on("click", function () {
-    alert("1");
-    $.ajax({
-        timeout: g_timeout,
-        type: "post",
-        dataType: "json",
-        url: action_url + "/demo/setValue",
-        data: {},
-        success: function (data) {
-            if(data == "success"){
-                $.alert(data);
+$(function () {
+    $("#loginBtn").on("click", function () {
+        alert("1");
+        $.ajax({
+            timeout: g_timeout,
+            type: "post",
+            dataType: "json",
+            url: action_url + "/demo/setValue",
+            data: {},
+            success: function (data) {
+                if (data == "success") {
+                    $.alert(data);
+                }
+            },
+            error: function () {
             }
-        },
-        error: function () {
-        }
+        });
     });
 });
